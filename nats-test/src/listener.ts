bf8d7cf44d57fdb82aabef5a27c96cdfa24fe484
safeerkhattak <safeerahmad5454@gmail.com>
2026-06-13 03:59:57 +0500

    feat: nats server app(learning shot nat server)

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,33 @@
+import nats, { Message, Stan } from "node-nats-streaming";
+
+console.clear();
+const id = Math.floor(Math.random() * 10000);
+const stan = nats.connect("ticketing", String(id), {
+  url: "http://localhost:4222",
+}) as any;
+
+stan.on("connect", () => {
+  console.log("Listener connected to NATS");
+
+  stan.on("close", () => {
+    console.log("NATS CONNECTION CLOSED");
+    process.exit();
+  });
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "orders-service-queue-group",
+    options,
+  );
+  subscription.on("message", (msg: Message) => {
+    const data = msg.getData();
+    if (typeof data === "string") {
+      console.log(`Recieved Event #${msg.getSequence()}, with data: ${data} `);
+    }
+    msg.ack();
+  });
+});
+
+process.on("SIGINT", () => stan.close()); //interupt it will close our client on interrupt same for below
+process.on("SIGTERM", () => stan.close()); // terminate 
+// if you close the node isntance forcefully from task manager the above close event wont occure also it does not 100% work on windows
