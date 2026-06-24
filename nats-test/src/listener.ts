cb0e49c8c8b558eb0cc8ea80c168a479af27fa5f
safeerkhattak <safeerahmad5454@gmail.com>
2026-06-24 14:28:16 +0500

    chore: written a listener class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -13,24 +13,26 @@ stan.on("connect", () => {
     console.log("NATS CONNECTION CLOSED");
     process.exit();
   });
-  const options = 
-      stan.subscriptionOptions()
-      .setManualAckMode(true)
-      .setDeliverAllAvailable() // to get all events deliver again
-      .setDurableName('accounting service');   /// to create durable subscription(keep track of either listener handles the event or not)
+  new TicketCreatedListener(stan).listen();
+  // commented we already done this in listener class 
+  // const options = 
+  //     stan.subscriptionOptions()
+  //     .setManualAckMode(true)
+  //     .setDeliverAllAvailable() // to get all events deliver again
+  //     .setDurableName('accounting service');   /// to create durable subscription(keep track of either listener handles the event or not)
 
-  const subscription = stan.subscribe(
-    "ticket:created",
-    "orders-service-queue-group", //
-    options,
-  );
-  subscription.on("message", (msg: Message) => {
-    const data = msg.getData();
-    if (typeof data === "string") {
-      console.log(`Recieved Event #${msg.getSequence()}, with data: ${data} `);
-    }
-    msg.ack();
-  });
+  // const subscription = stan.subscribe(
+  //   "ticket:created",
+  //   "orders-service-queue-group", //
+  //   options,
+  // );
+  // subscription.on("message", (msg: Message) => {
+  //   const data = msg.getData();
+  //   if (typeof data === "string") {
+  //     console.log(`Recieved Event #${msg.getSequence()}, with data: ${data} `);
+  //   }
+  //   msg.ack();
+  // });
 });
 
 process.on("SIGINT", () => stan.close()); //interupt it will close our client on interrupt same for below
@@ -47,3 +49,55 @@ process.on("SIGTERM", () => stan.close()); // terminate
 
 
 //3 more solution that will not work to solve the concurrency issue
+
+
+abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract  onMessage(data:any,msg:Message):void;
+  private client: Stan;
+  protected ackwait = 5* 1000;
+  
+
+  constructor(client: Stan){
+    this.client = client
+  }
+
+  subscriptionOptions(){
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setAckWait(this.ackwait)
+      .setDurableName(this.queueGroupName)
+  }
+
+  listen(){
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions(),
+    );
+    subscription.on('message',(msg:Message)=>{
+      console.log(`Message recieved: ${this.subject}/${this.queueGroupName}`)
+      
+      const parseData = this.parseMessage(msg);
+      this.onMessage(parseData,msg)
+    })
+  }
+  parseMessage(msg:Message){
+    const data = msg.getData()
+    return typeof data === 'string'
+      ? JSON.parse(data)
+      : JSON.parse(data.toString('utf8'))
+  }
+}
+
+class TicketCreatedListener extends Listener{
+   subject = 'ticket:created';
+   queueGroupName = 'payment-service';
+   onMessage(data: any, msg: Message): void {
+      console.log("Event data!",data)
+      msg.ack()
+   }
+
+}
